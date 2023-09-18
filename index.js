56a0c9c1731d97febfe3da3d18f45793dd2e8ed5
isaac kiplangat <isaackiplangat1999@gmail.com>
2023-09-18 12:07:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let reply;
+  if (  400 >= distance){
+      reply = "This one is on me!"
+    }
+    else if (distance > 2000 && distance < 2500){
+      reply ='I will gladly take your thirty bucks.'}
+    else if (2500 < distance){
+      reply = 'No can do.'
+    }
+    return reply
+  }
+consolescuberGreetingForFeet(200)
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let reply;
+  if (city == "NYC"){
+    reply= "Ok, sounds good."
+  } else if (city == "Pittsburgh"){
+    reply= "No go."
+  }
+  return reply
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let reply
+  if (tip == "generous"){
+    reply= "Thank you so much."
+  } else if (tip == 'not as generous' ){
+    reply ="Thank you."
+  }
+  else {
+    reply = "Bye."
+  }
+  return reply
 }
